b178a44a1cdd2135266628d49b54c9d91e4f36ce
fixiong <31613128+fixiong@users.noreply.github.com>
2018-07-30 03:37:02 +0800

    Update possion_stiching.h

--- possion_stiching.h
+++ possion_stiching.h
@@ -9,7 +9,7 @@ void poisson_stiching(
 	const bear::PImage &src2,
 	const bear::PImage &mask,
 	unsigned int format,
-	unsigned int iteration_time,
+	unsigned int iteration_time = 10,
 	int base_level = 0);
 
 #endif
