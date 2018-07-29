047af632d4ed64ac217f02b484cd235d7b05489e
fixiong <31613128+fixiong@users.noreply.github.com>
2018-07-30 03:38:00 +0800

    Update possion_solver.h

--- possion_solver.h
+++ possion_solver.h
@@ -7,7 +7,7 @@ void dxy_poisson_solver(
 	const bear::PImage &dst,
 	const bear::PImage &dx,
 	const bear::PImage &dy,
-	unsigned int iteration_time,
+	unsigned int iteration_time = 10,
 	int base_level = 0);
 
 #endif
