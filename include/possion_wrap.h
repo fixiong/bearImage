ff1944f1383392efa7f45ffa84430a37322ff171
fixiong <fifixiong@hotmail.com>
2019-04-15 16:05:04 +0800

    update

--- include/possion_wrap.h
+++ include/possion_wrap.h
@@ -1,6 +1,8 @@
 #ifndef _POSSION_WRAP_H
 #define _POSSION_WRAP_H
 
+#include <utility>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -8,11 +10,11 @@ extern "C" {
 	typedef void* matImagePtr;
 	void go_poisson_stiching(imagePtr dstPtr, matImagePtr srcPtr,unsigned int rd,unsigned int format);
 
-	imagePtr newImagePtr(size_t width, size_t height, size_t channel_size, unsigned int eletype, size_t ele_size, char* data, size_t width_step);
+	imagePtr newImagePtr(std::size_t width, std::size_t height, std::size_t channel_size, unsigned int eletype, std::size_t ele_size, char* data, std::size_t width_step);
 	void freeImagePtr(imagePtr p);
 
 	matImagePtr newMatImagePtr(unsigned int y_max);
-	void matPushImage(matImagePtr m,unsigned int y, size_t width, size_t height, size_t channel_size, unsigned int eletype, size_t ele_size, char* data, size_t width_step);
+	void matPushImage(matImagePtr m,unsigned int y, std::size_t width, std::size_t height, std::size_t channel_size, unsigned int eletype, std::size_t ele_size, char* data, std::size_t width_step);
 	void freeMat(matImagePtr m);
 
 
