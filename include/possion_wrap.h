fa7d260b482a7cc781e3eb4c6e431c44aaf25e63
fixiong <fifixiong@hotmail.com>
2019-11-09 18:55:16 +0800

    update

--- include/possion_wrap.h
+++ include/possion_wrap.h
@@ -1,16 +1,18 @@
 #ifndef _POSSION_WRAP_H
 #define _POSSION_WRAP_H
+#include "../include/bear/dynamic_image.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 	typedef void* imagePtr;
 	typedef void* matImagePtr; 
-	void go_poisson_stiching(imagePtr dstPtr, matImagePtr srcPtr, matImagePtr borderPtr, unsigned int rd,unsigned int format, unsigned int mode);
+	unsigned int assimilate(const char* workDirPath,const char* fileName, unsigned int tNumX, unsigned int tNumY, unsigned int mode, unsigned int param);
+	void go_poisson_stiching(bear::dynamic_image_ptr dst, std::vector<std::vector<bear::dynamic_image_ptr>>  src, std::vector<std::vector<bear::dynamic_image_ptr>> border, unsigned int rd, unsigned int format, unsigned int mode);
 
 	imagePtr newImagePtr(unsigned long long width, unsigned long long height, unsigned long long channel_size, unsigned int eletype, unsigned long long ele_size, char* data, unsigned long long width_step);
 	void freeImagePtr(imagePtr p);
-
+	
 	matImagePtr newMatImagePtr(unsigned int y_max);
 	unsigned int matPushImage(matImagePtr m,unsigned int y, unsigned long long width, unsigned long long height, unsigned long long channel_size, unsigned int eletype, unsigned long long ele_size, char* data, unsigned long long width_step);
 	void freeMat(matImagePtr m);
